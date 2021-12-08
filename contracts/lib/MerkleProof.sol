7f244a8ccc9df8dfcf92a0bb828e35f4088c733f
hrishikeshio <hrishikesh911@gmail.com>
2021-12-08 11:41:37 +0400

    skip hash working

--- contracts/lib/MerkleProof.sol
+++ contracts/lib/MerkleProof.sol
@@ -7,49 +7,85 @@ import "hardhat/console.sol";
  */
 library MerkleProof {
 
-    function verifyMultiple(bytes32[][] memory proofs, bytes32 root, bytes32 [] memory leaves, uint32[] memory assetId, uint32 depth) internal view returns (bool) {
+    function verifyMultiple(bytes32[][] memory proofs, bytes32 root, bytes32 [] memory leaves, uint32[] memory assetId, uint32 depth, uint32 maxAssets) internal view returns (bool) {
          for (uint256 i =0; i< proofs.length; i++)
          {
-             if(!verify(proofs[i], root, leaves[i], assetId[i], depth)) return false;
-             //console.log('passed for', i+1);
+             if(!verify(proofs[i], root, leaves[i], assetId[i], depth, maxAssets)) return false;
+             ////console.log('passed for', i+1);
          }
          return true;
     }
-    
+
     /**
      * @dev Returns true if a `leaf` can be proved to be a part of a Merkle tree
      * defined by `root`. For this, a `proof` must be provided, containing
      * sibling hashes on the branch from the leaf to the root of the tree. Each
      * pair of leaves and each pair of pre-images are assumed to be sorted.
      */
-    function verify(bytes32[] memory proof, bytes32 root, bytes32 leaf, uint32 assetId, uint32 depth) internal view returns (bool) {
+    function verify(bytes32[] memory proof, bytes32 root, bytes32 leaf, uint32 assetId, uint32 depth, uint32 maxAssets) internal view returns (bool) {
         bytes32 computedHash = leaf;
         bytes memory seq = bytes(getSequence(assetId, depth));
         //console.log(string(seq));
+
+        uint256 last_node = maxAssets;
+        uint256 my_node = assetId;
         uint256 j = depth;
-        for (uint256 i = 0; i < proof.length; i++) {
+        uint256 i =0;
+        // for (uint256 i = 0; i < proof.length; i++) {
+          while(j>0) {
             bytes32 proofElement = proof[i];
-            //console.logBytes1(seq[depth - i - 1]);
+            ////console.logBytes1(seq[depth - i - 1]);
+            //console.log('proofElement');
+
             //console.logBytes32(proofElement);
+            //console.log('computedHash');
+            //console.logBytes32(computedHash);
             j--;
-            if(proofElement != 0x0) {
+            //console.log(i,j);
+            // if(proofElement != 0x0) {
+            //console.log('my_node',my_node);
+            //console.log('last_node',last_node);
+            //console.log('last_node%2',last_node%2);
+            //console.log('last_node%2!=1',last_node%2!=1);
+              //console.log('last_node!=my_node', last_node!=my_node);
+              //console.log('seq[j]');
+              //console.logBytes1(seq[j]);
+
+              //skip proof check  if my node is  last node and number of nodes on level is odd
+              if(last_node%2==1 && last_node==my_node) {
+
+                    my_node = my_node/2 + my_node%2 ;
+                    last_node= last_node/2 + last_node%2 ;
+                        continue;
+              }
+                //console.log('in seq[j]');
+                //console.logBytes1(seq[j]);
                 if (seq[j] == 0x30) {
-                    //console.log('Hash(current computed hash + current element of the proof)');
+                    ////console.log('Hash(current computed hash + current element of the proof)');
                     computedHash = keccak256(abi.encodePacked(computedHash, proofElement));
                 } else {
-                    //console.log('Hash(current element of the proof + current computed hash)');
+                    ////console.log('Hash(current element of the proof + current computed hash)');
                     computedHash = keccak256(abi.encodePacked(proofElement, computedHash));
                 }
-            }
+                i++;
+
+            my_node = my_node/2 + my_node%2 ;
+            last_node= last_node/2 + last_node%2 ;
+
+            // Check if the computed hash (root) is equal to the provided root
+
+            //console.log('computedHash');
+            //console.logBytes32(computedHash);
+            //console.log('root');
+            //console.logBytes32(root);
         }
 
-        // Check if the computed hash (root) is equal to the provided root
         return computedHash == root;
     }
 
     function getSequence(uint256 assetId, uint256 depth) internal pure returns (bytes memory)
     {
-     
+
         bytes memory output = new bytes(depth);
         uint256 n = assetId - 1;
         for (uint8 i = 0; i < depth; i++) {
@@ -60,13 +96,13 @@ library MerkleProof {
         //string memory x = string(output);
         // for(uint8 i = 0; i < depth ; i++)
         // {
-        //     console.log(uint8(output[i]));
+        //     //console.log(uint8(output[i]));
         // }
         return output;
     }
 }
 
-// Number to Binary. 
+// Number to Binary.
 // And how we travese that binary .
 // Depth log ? | check if its attack
 
@@ -75,7 +111,7 @@ library MerkleProof {
 //      0
 //    0
 //  0  0   0
- 
+
 //  00
 //  01
 //  1
@@ -93,9 +129,9 @@ library MerkleProof {
 // 11
 
 //          0
-//       0         
+//       0
 //    0      0
-//  0  0   0   0    0 
+//  0  0   0   0    0
 
 // 0,0
 // 0.1
@@ -109,18 +145,18 @@ library MerkleProof {
 //  100
 
 
-  
+
 
 //           0
-//       0               
-//    0      0       0     
-//  0  0   0   0    0  0    
+//       0
+//    0      0       0
+//  0  0   0   0    0  0
 // 000 001 010 011  100 101
 
 // 1(0)0 = 10
 // 1(0)1 = 11
 
-// 0 : right compute 
+// 0 : right compute
 // 1 : left compute
 
 
@@ -138,6 +174,6 @@ library MerkleProof {
 // 000   001 010 011  100 101
 
 
-// RipeMd 160 + position 
+// RipeMd 160 + position
 // User provides the proof
 // Pass a dummy proof in between for not existing node
--- test/MerkleProof.spec.js
+++ test/MerkleProof.spec.js
@@ -42,12 +42,12 @@ function getProofPath(assetId, excludeParent, compact) {
   for (let currentLevel = tree.length - 1; currentLevel > 0; currentLevel--) {
     const currentLevelNodes = tree[currentLevel];
     const currentLevelCount = currentLevelNodes.length;
-    // console.log('localCurrentlevelNodes', currentLevelNodes);
+    // //console.log('localCurrentlevelNodes', currentLevelNodes);
     // if this is an odd end node to be promoted up, skip to avoid proofs with null values
     if (index === currentLevelCount - 1 && currentLevelCount % 2 === 1) {
       index = Math.floor(index / 2);
       //0xccd4ae6ef6fa5c628d5b3e95d10589f3cbb4e21e9f997aeda1c7403b267bac1a
-      compactProofPath.push('0x0000000000000000000000000000000000000000000000000000000000000000');
+      // compactProofPath.push('0x0000000000000000000000000000000000000000000000000000000000000000');
       // eslint-disable-next-line no-continue
       continue;
     }
@@ -90,8 +90,8 @@ describe('MerkleProof', function () {
     // const votes = [100, 200, 300, 400, 500];
 
     // await createMerkle(votes);
-    // console.log('NewTree');
-    // console.log(tree);
+    // //console.log('NewTree');
+    // //console.log(tree);
     // const tree1 = merkle('keccak256').sync(votes);
 
     // const proof1 = getProofPath(1, true, true);
@@ -100,20 +100,20 @@ describe('MerkleProof', function () {
     // const proof4 = getProofPath(4, true, true);
     // const proof5 = getProofPath(5, true, true);
     // // const proof2 = tree1.getProofPath(0, true, true);
-    // // console.log(tree1.depth());
-    // // console.log(proof);
-    // // console.log(proof2);
+    // // //console.log(tree1.depth());
+    // // //console.log(proof);
+    // // //console.log(proof2);
 
-    // console.log(proof3);
-    // console.log('Positive 000', await MerkleProof.verify(proof1, tree[0][0], ethers.utils.solidityKeccak256(['uint256'], [100]), 1, 3));
-    // console.log('Negagtive 000', await MerkleProof.verify(proof1, tree[0][0], ethers.utils.solidityKeccak256(['uint256'], [100]), 2, 3));
-    // console.log('Negative 000', await MerkleProof.verify(proof1, tree[0][0], ethers.utils.solidityKeccak256(['uint256'], [100]), 3, 3));
-    // console.log('Positive 001', await MerkleProof.verify(proof2, tree[0][0], ethers.utils.solidityKeccak256(['uint256'], [200]), 2, 3));
-    // console.log('Positive 010', await MerkleProof.verify(proof3, tree[0][0], ethers.utils.solidityKeccak256(['uint256'], [300]), 3, 3));
-    // console.log('Positive 011', await MerkleProof.verify(proof4, tree[0][0], ethers.utils.solidityKeccak256(['uint256'], [400]), 4, 3));
-    // console.log('Positive 100', await MerkleProof.verify(proof5, tree[0][0], ethers.utils.solidityKeccak256(['uint256'], [500]), 5, 3));
+    // //console.log(proof3);
+    // //console.log('Positive 000', await MerkleProof.verify(proof1, tree[0][0], ethers.utils.solidityKeccak256(['uint256'], [100]), 1, 3));
+    // //console.log('Negagtive 000', await MerkleProof.verify(proof1, tree[0][0], ethers.utils.solidityKeccak256(['uint256'], [100]), 2, 3));
+    // //console.log('Negative 000', await MerkleProof.verify(proof1, tree[0][0], ethers.utils.solidityKeccak256(['uint256'], [100]), 3, 3));
+    // //console.log('Positive 001', await MerkleProof.verify(proof2, tree[0][0], ethers.utils.solidityKeccak256(['uint256'], [200]), 2, 3));
+    // //console.log('Positive 010', await MerkleProof.verify(proof3, tree[0][0], ethers.utils.solidityKeccak256(['uint256'], [300]), 3, 3));
+    // //console.log('Positive 011', await MerkleProof.verify(proof4, tree[0][0], ethers.utils.solidityKeccak256(['uint256'], [400]), 4, 3));
+    // //console.log('Positive 100', await MerkleProof.verify(proof5, tree[0][0], ethers.utils.solidityKeccak256(['uint256'], [500]), 5, 3));
 
-    // console.log(await MerkleProof.getSequence(5, 3));
+    // //console.log(await MerkleProof.getSequence(5, 3));
 
     // const proofs = [proof1, proof2, proof3, proof4, proof5];
     // const leaves = [ethers.utils.solidityKeccak256(['uint256'], [100]),
@@ -122,7 +122,7 @@ describe('MerkleProof', function () {
     //   ethers.utils.solidityKeccak256(['uint256'], [400]),
     //   ethers.utils.solidityKeccak256(['uint256'], [500])];
     // const assetIds = [1, 2, 3, 4, 5];
-    // console.log(await MerkleProof.verifyMultiple(proofs, tree[0][0], leaves, assetIds, 3));
+    // //console.log(await MerkleProof.verifyMultiple(proofs, tree[0][0], leaves, assetIds, 3));
   });
 
   it('Tree from 2 nodes to 11 nodes', async function () {
@@ -139,15 +139,17 @@ describe('MerkleProof', function () {
       let assetIds = [];
       let leaves = [];
       const depth = Math.log2(i) % 1 === 0 ? Math.log2(i) : Math.ceil(Math.log2(i));
-      //console.log('depth', depth);
-      //console.log(getProofPath(6, true, true));
+      ////console.log('depth', depth);
+      ////console.log(getProofPath(6, true, true));
       for (let j = 0; j < i; j++) {
         proofs.push(getProofPath(j + 1, true, true));
         leaves.push(ethers.utils.solidityKeccak256(['uint256'], [votes[j]]));
         assetIds.push(j + 1);
       }
-      //console.log(proofs, leaves, assetIds);
-      console.log(await MerkleProof.verifyMultiple(proofs, tree[0][0], leaves, assetIds, depth));
+      ////console.log(proofs, leaves, assetIds);
+      //console.log(proofs);
+      //console.log('maxAssets', i);
+      console.log(await MerkleProof.verifyMultiple(proofs, tree[0][0], leaves, assetIds, depth, i));
     }
   });
 });
